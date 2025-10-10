8845372910881638536dd4297f9c24653d050e80
jefrin <jefrinraj07@gmail>
2025-10-10 13:56:45 +0530

    Commit

--- BookStore/obj/Debug/net9.0/BookStore.AssemblyInfo.cs
+++ BookStore/obj/Debug/net9.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58a64002a654052a586b944bdcc496c2b51068b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6ac360eb1fa188f9136aaf3e7e31c85ffc8707")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
